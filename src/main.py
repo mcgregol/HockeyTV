ced9acc575599cfbff8d783930213b5c9e73e681
mcgregol <mcgregol@salinehornets.org>
2022-05-12 12:40:42 -0400

    implemented webdriverwait

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from seleniumwire import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from yt_dlp import YoutubeDL
 import getpass, time
 
@@ -13,13 +14,20 @@ class User:
     def run(self):
         browser = webdriver.Chrome()
         browser.get('https://www.hockeytv.com/login')
+
         email_input = browser.find_element(By.XPATH, '//*[@id="emailInput"]')
         passwd_input = browser.find_element(By.XPATH, '//*[@id="passwordInput"]')
+        sign_in = browser.find_element(By.XPATH, '/html/body/app-root/section/div/div[2]/app-login/div/div/form/button')
+
         email_input.send_keys(self.user)
         passwd_input.send_keys(self.passwd)
-        time.sleep(10)
+        sign_in.click()
+
+        wait = WebDriverWait(browser, 10)
+        search_wait = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="term"]')))
+
+        time.sleep(5)
         browser.quit()
-        ## implement webdriver wait on search bar
 
 my_user = User(input("Enter username: "), getpass.getpass("Enter password: "))
 my_user.run()
